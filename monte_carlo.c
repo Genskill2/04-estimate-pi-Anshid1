4c587bd20caeb98e17f6427186364d394967464a
Anshid1 <84650114+Anshid1@users.noreply.github.com>
2021-06-19 23:34:16 +0530

    Solution

--- monte_carlo.c
+++ monte_carlo.c
@@ -38,22 +38,23 @@ int main(void) {
     }
   }
 }
-float mc_pi(int x)
-{
-  int c=0,s=x;
-  for(int i=0;i<n;i++){
-    float x,y,len;
-    x=frandom();
-    y=frandom();
-    len=x*x+y*y;
-    if(len<-1)
-    {
-      c++;
-    }
-    float pi=4.0*c/s;
-    return pi:
-  }
-  
+
+  float mc_pi(int n)
+{	
+	int pc=0,ps=n;
+	
+	for(int i=0; i<n; i++){
+	float x,y,len;
+	x=frandom();
+	y=frandom();
+	len =x*x+y*y;
+	if(len<=1){
+	pc++;
+	}
+	}
+	float pi = 4.0*pc/ps;
+	return pi;
+}
 
 
 
